794312690d56c91c2d0378433ae1aacd27c852a4
jonathan <776177850he@gmail.com>
2021-07-07 14:22:03 -0500

    dumbo mistake number 2

--- crop.py
+++ crop.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-def crop(orig_img, hand_dets, scale):
+def crop(orig_img, hand_dets, scale, diagnostics = False):
     SMALLEST_SIZE = 256
     
     #assign the edges of the bounding box based on hand_dets
@@ -12,7 +12,7 @@ def crop(orig_img, hand_dets, scale):
     #define the side length based on width and height
     width = abs(xmin-xmax)
     height = abs(ymin-ymax)
-    side = max(width,height)*scale
+    side = int(max(width,height)*scale)
     side = max(side, SMALLEST_SIZE)
 
     #define the center of the bounding box
@@ -25,6 +25,10 @@ def crop(orig_img, hand_dets, scale):
     upper_edge = max(0, int(ycenter - side/2))
     bottom_edge = min(len(orig_img[0])-1, int(ycenter + side/2 + 1))
 
-    img = orig_img[left_edge:right_edge,upper_edge:bottom_edge].copy()
+    img = orig_img[upper_edge:bottom_edge,left_edge:right_edge].copy()
 
-    return cv.resize(img,(SMALLEST_SIZE,SMALLEST_SIZE),interpolation = cv.INTER_AREA)
+    if diagnostics:
+        print("xcenter:"+str(xcenter)+" ycenter:"+str(ycenter)+" side:"+str(side))
+        print("img["+str(upper_edge)+":"+str(bottom_edge)+", "+str(left_edge)+":"+str(right_edge)+"]")    
+
+    return cv.resize(img,(side,side),interpolation = cv.INTER_AREA)
