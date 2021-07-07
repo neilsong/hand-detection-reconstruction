22be7ffa4ae30fe7adad221adea153c443f83ed5
jonathan <776177850he@gmail.com>
2021-07-07 12:32:51 -0500

    it works?

--- crop.py
+++ crop.py
@@ -3,25 +3,28 @@ import cv2 as cv
 def crop(orig_img, hand_dets, scale):
     SMALLEST_SIZE = 256
     
+    #assign the edges of the bounding box based on hand_dets
     xmin = hand_dets[0]
     ymin = hand_dets[1]
     xmax = hand_dets[2]
     ymax = hand_dets[3]
 
+    #define the side length based on width and height
     width = abs(xmin-xmax)
     height = abs(ymin-ymax)
     side = max(width,height)*scale
     side = max(side, SMALLEST_SIZE)
 
-    xcenter = int((xmax+xmin+1)/2)
-    ycenter = int((ymax+ymin+1)/2)
+    #define the center of the bounding box
+    xcenter = (xmax+xmin)/2
+    ycenter = (ymax+ymin)/2
     
-    left_edge = max(0, xcenter - int(side/2))
-    right_edge = min(len(orig_img), xcenter + int((side+1)/2))
-    upper_edge = max(0, ycenter - int(side/2))
-    bottom_edge = min(len(orig_img[0]), ycenter + int((side+1)/2))
+    #define the edges of the cropped image
+    left_edge = max(0, int(xcenter - side/2))
+    right_edge = min(len(orig_img)-1, int(xcenter + side/2 + 1))
+    upper_edge = max(0, int(ycenter - side/2))
+    bottom_edge = min(len(orig_img[0])-1, int(ycenter + side/2 + 1))
 
     img = orig_img[left_edge:right_edge,upper_edge:bottom_edge].copy()
 
     return cv.resize(img,(SMALLEST_SIZE,SMALLEST_SIZE),interpolation = cv.INTER_AREA)
-    
