d5683cba36cc7e917f30ecf231353ce4223ebd6b
jonathan <776177850he@gmail.com>
2021-07-06 20:40:29 -0500

    solved the first dumbo mistake

--- crop.py
+++ crop.py
@@ -1,4 +1,4 @@
-import cv2 as cv
+#import cv2 as cv
 
 def crop(orig_img, hand_dets, scale):
     SMALLEST_SIZE = 256
@@ -7,7 +7,8 @@ def crop(orig_img, hand_dets, scale):
     ymin = hand_dets[1]
     xmax = hand_dets[2]
     ymax = hand_dets[3]
-    
+    print(xmin+" "+xmax)
+    print(ymin+" "+ymax)
     width = abs(xmin-xmax)
     height = abs(ymin-ymax)
     side = max(width,height)*scale
@@ -22,5 +23,29 @@ def crop(orig_img, hand_dets, scale):
     bottom_edge = min(len(orig_img[0]), ycenter + int((side+1)/2))
 
     img = orig_img[left_edge:right_edge,upper_edge:bottom_edge].copy()
-
+    print(left_edge+" "+right_edge)
+    print(upper_edge+" "+bottom_edge)
     return cv.resize(img,(SMALLEST_SIZE,SMALLEST_SIZE),interpolation = cv.INTER_AREA)
+
+xmin = 220
+xmax = 230
+ymin = 220
+ymax = 230
+scale = 2.0
+
+SMALLEST_SIZE = 256
+width = abs(xmin-xmax)
+height = abs(ymin-ymax)
+side = max(width,height)*scale
+side = max(side, SMALLEST_SIZE)
+
+xcenter = int((xmax+xmin+1)/2)
+ycenter = int((ymax+ymin+1)/2)
+
+left_edge = max(0, xcenter - int(side/2))
+right_edge = min(500, xcenter + int((side+1)/2))
+upper_edge = max(0, ycenter - int(side/2))
+bottom_edge = min(500, ycenter + int((side+1)/2))
+
+print(str(left_edge)+" "+str(right_edge))
+print(str(upper_edge)+" "+str(bottom_edge))
